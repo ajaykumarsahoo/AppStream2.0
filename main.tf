a3f52702db67983df66f74a2f21b31276dbb1513
Ajay Kumar Sahoo <ajkumar.brm@gmail.com>
2025-07-17 22:37:56 +0530

    Updated module appsteam image for appstream

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "appstream" {
   # Required variables (replace with your values)
   fleet_name    = "example-fleet"
   instance_type = "stream.standard.small"
+  appstream_image = "Amazon-AppStream2-Sample-Image-06-17-2024"
   #image_name        = "AppStream-WinServer2019-05-30-2025"
   stack_name = "example-stack"
   session_type = "multi_session"
